927342a6397eb6ae3755d91d6c190a9873d9a873
Michael Neu <git@michaeln.eu>
2018-06-13 21:56:15 +0200

    create BasicProvider

--- src/providers/index.ts
+++ src/providers/index.ts
@@ -2,3 +2,13 @@ export interface Provider {
   bootstrap() : Promise<void>;
   shutdown() : Promise<void>;
 }
+
+export abstract class BasicProvider implements Provider {
+  public async bootstrap() : Promise<void> {
+    return;
+  }
+
+  public async shutdown() : Promise<void> {
+    return;
+  }
+}
