cbf69240ac698515afb145c603fc3e30ff6dfbd1
Michael Neu <git@michaeln.eu>
2018-06-13 13:15:19 +0200

    create main application

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,33 @@
-console.log("accommodate");
+import { Container, Service as Injectable } from "typedi";
+import { Service } from "./services";
+import { PersistenceService } from "./services/persistence";
+import { WebService } from "./services/web";
+
+@Injectable()
+class Application implements Service {
+  private readonly services: Service[];
+
+  constructor(persistence: PersistenceService, web: WebService) {
+    this.services = [
+      persistence,
+      web,
+    ];
+  }
+
+  public async bootstrap() : Promise<void> {
+    for (const service of this.services) {
+      await service.bootstrap();
+    }
+  }
+
+  public async shutdown() : Promise<void> {
+    for (const service of this.services) {
+      await service.shutdown();
+    }
+  }
+}
+
+(async () => {
+  const server = Container.get<Service>(Application);
+  await server.bootstrap();
+})();
