1737bee66136f9f4ff949952487094d68f8240d5
aulonashabani <aulonashabani@gmail.com>
2018-06-18 16:11:56 +0200

    prevent unauthorized access except for signup/login

--- src/providers/jwt.ts
+++ src/providers/jwt.ts
@@ -1,3 +1,4 @@
+import { RequestHandler } from "express";
 import * as jwt from "express-jwt";
 import { sign } from "jsonwebtoken";
 import { Service } from "typedi";
@@ -9,10 +10,15 @@ export class JWTProvider extends BasicProvider {
     return process.env.JWT_SECRET || "insecure_default_secret";
   }
 
-  public getExpressMiddleware() : jwt.RequestHandler {
+  public getExpressMiddleware() : RequestHandler {
     return jwt({
-      credentialsRequired: false,
+      credentialsRequired: true,
       secret: this.secret,
+    }).unless({
+      path: [
+        "/users/signup",
+        "/users/login",
+      ],
     });
   }
 
