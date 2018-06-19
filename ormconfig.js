97e42ea50352cdf7486f47a8ec1508102d3adcf1
Michael Neu <git@michaeln.eu>
2018-06-19 18:45:51 +0200

    fix typeorm entities not found at runtime

--- ormconfig.js
+++ ormconfig.js
@@ -1,16 +1,22 @@
+const isDevelopment = process.env.NODE_ENV !== "production";
+
+const entities = isDevelopment ? "src/entity/**/*.ts" : "dist/entity/**/*.js";
+const migration = isDevelopment ? "src/migration/**/*.ts" : "dist/migration/**/*.js";
+const subscriber = isDevelopment ? "src/subscriber/**/*.ts" : "dist/subscriber/**/*.js";
+
 module.exports = {
   type: "mongodb",
   url: process.env.MONGODB_URI,
   synchronize: true,
   logging: false,
   entities: [
-    "dist/entity/**/*.js",
+    entities,
   ],
   migrations: [
-    "dist/migration/**/*.js",
+    migration,
   ],
   subscribers: [
-    "dist/subscriber/**/*.js",
+    subscriber,
   ],
   cli: {
     entitiesDir: "src/entity",
