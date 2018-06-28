6cd40289bc4d1305f608f17c7bb43b30419156f5
aulonashabani <aulonashabani@gmail.com>
2018-06-28 23:07:04 +0200

    add accommodation image upload

--- src/controllers/images.ts
+++ src/controllers/images.ts
@@ -1,8 +1,9 @@
 import { Request } from "express";
-import { JsonController, Post, Req, UploadedFile } from "routing-controllers";
+import { BodyParam, JsonController, Post, Req, UploadedFile } from "routing-controllers";
 import { Repository } from "typeorm";
 import { InjectRepository } from "typeorm-typedi-extensions";
 import { BaseController } from ".";
+import { LandlordBiography } from "../entity/LandlordBiography";
 import { TenantBiography } from "../entity/TenantBiography";
 import { User, UserMode } from "../entity/User";
 import { CloudinaryProvider } from "../providers/cloudinary";
@@ -18,20 +19,49 @@ export class ImagesController extends BaseController {
   }
 
   @Post("/profile")
-  public async upload(@Req() request: Request, @UploadedFile("image") uploadedImage: any) {
+  public async uploadProfileImage(@Req() request: Request, @UploadedFile("image") uploadedImage: any) {
     const user = await this.getUserFromRequest(request);
 
-    const { buffer, originalname } = uploadedImage;
-    const image = await this.cloudinary.upload(buffer);
-
     if (user.mode === UserMode.Tenant) {
-      user.bio.images.push(image.url);
+      const image = await this.uploadImage(uploadedImage);
+      const bio = user.bio as TenantBiography;
+
+      bio.images.push(image.url);
       await this.users.save(user);
+
+      return image.url;
     }
 
-    return {
-      name: originalname,
-      url: image.url,
-    };
+    throw new Error("not a tenant");
+  }
+
+  @Post("/accommodation")
+  public async uploadAccommodationImage(@Req() request: Request, @UploadedFile("image") uploadedImage: any, @BodyParam("id") id: string) {
+    const user = await this.getUserFromRequest(request);
+
+    if (user.mode === UserMode.Landlord) {
+      const bio = user.bio as LandlordBiography;
+
+      for (const estate of bio.realEstates) {
+        if (estate.id.toHexString() === id) {
+          const image = await this.uploadImage(uploadedImage);
+
+          estate.images.push(image.url);
+          await this.users.save(user);
+
+          return image.url;
+        }
+      }
+
+      throw new Error("no such real estate");
+    }
+
+    throw new Error("not a landlord");
+  }
+
+  private async uploadImage(image: any) {
+    const { buffer } = image;
+
+    return await this.cloudinary.upload(buffer);
   }
 }
