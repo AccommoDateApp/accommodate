f3fc1727751409cb352c6b6d06bef3cdfee9b58f
tomasruizt <tomas.ruiz.te@gmail.com>
2018-06-30 23:50:16 +0200

    Serve the matching endpoints

--- src/controllers/matching.ts
+++ src/controllers/matching.ts
@@ -4,8 +4,11 @@ import { BodyParam, Get, JsonController, Post, Req } from "routing-controllers";
 import { Repository } from "typeorm";
 import { InjectRepository } from "typeorm-typedi-extensions";
 import { UserMode } from "../entity/Biography";
+import { LandlordBiography } from "../entity/LandlordBiography";
 import { Match } from "../entity/Match";
 import { RealEstate } from "../entity/RealEstate";
+import { RejectedMatch } from "../entity/RejectedMatch";
+import { TenantBiography } from "../entity/TenantBiography";
 import { User } from "../entity/User";
 import { BaseController } from "./index";
 
@@ -13,22 +16,29 @@ import { BaseController } from "./index";
 export class MatchingController extends BaseController {
   constructor(
     @InjectRepository(User)
-    private readonly userRepo: Repository<User>,
+    protected readonly users: Repository<User>,
     @InjectRepository(Match)
     private readonly matchRepo: Repository<Match>,
-    @InjectRepository(RealEstate)
-    private readonly realEstateRepo: Repository<RealEstate>,
+    @InjectRepository(RejectedMatch)
+    private readonly rejectedMatchRepo: Repository<RejectedMatch>,
   ) {
-    super(userRepo);
+    super(users);
   }
 
   @Get("/")
-  public async getActualMatches(@Req() request: Request) {
-    const user = await this.getUserFromRequest(request);
-    if (user.mode === UserMode.Tenant) {
-      return await this.findTenantsMatches(user);
+  public async getUserMatches(@Req() request: Request) : Promise<UserMatches<any>> {
+    try {
+      const user = await this.getUserFromRequest(request);
+      if (user.mode === UserMode.Tenant) {
+        return await this.findTenantsUserMatches(user);
+      }
+      return await this.findLandlordsUserMatches(user);
+    } catch {
+      return {
+        actualMatches: [],
+        potentialMatches: [],
+      };
     }
-    return await this.findLandlordsMatches(user);
   }
 
   @Post("/create")
@@ -36,24 +46,88 @@ export class MatchingController extends BaseController {
     @BodyParam("realEstateID") realEstateID: string,
     @BodyParam("tenantUserID") tenantUserID: string,
   ) {
-    const match = this.matchRepo.create({
-      realEstateID: new ObjectId(realEstateID),
-      tenantUserID: new ObjectId(tenantUserID),
-    });
-    return await this.matchRepo.save(match);
+    const match = this.matchRepo.create();
+    match.realEstateID = new ObjectId(realEstateID);
+    match.tenantUserID = new ObjectId(tenantUserID);
+    await this.matchRepo.save(match);
+    return true;
   }
 
-  private async findTenantsMatches(user: User) {
-    const matches = await this.matchRepo.find({ tenantUserID: user.id });
-    return matches.map((match) => match.realEstateID);
+  @Post("/reject")
+  public async rejectMatch(
+    @BodyParam("realEstateID") realEstateID: string,
+    @BodyParam("tenantUserID") tenantUserID: string,
+  ) {
+    const match = this.rejectedMatchRepo.create();
+    match.realEstateID = new ObjectId(realEstateID);
+    match.tenantUserID = new ObjectId(tenantUserID);
+    await this.rejectedMatchRepo.save(match);
+    return true;
   }
 
-  // TODO: Test that we can get the landlord's matches as well!
-  private async findLandlordsMatches(landlord: User) {
-    return await this.matchRepo.find({
-      relations: ["realEstates", "matches"],
-      select: ["tenantUserID"],
-      where: { landlord },
-    });
+  private async findTenantsUserMatches(user: User) : Promise<UserMatches<RealEstate>> {
+    const allMatches = await this.matchRepo.find();
+    const actualMatches = allMatches.filter(
+      (match) => new ObjectId(match.tenantUserID).toHexString() === new ObjectId(user.bio.id).toHexString(),
+    );
+
+    const actualRealEstateIDs = new Set(actualMatches.map(
+      (match) => new ObjectId(match.realEstateID).toHexString()),
+    );
+
+    const allLandlords = await this.users.find({ mode: UserMode.Landlord });
+    const allRealEstates: RealEstate[] = [];
+    for (const landlord of allLandlords) {
+      allRealEstates.push(...(landlord.bio as LandlordBiography).realEstates);
+    }
+
+    const actualRealEstates = allRealEstates
+      .filter((realEstate) => actualRealEstateIDs.has(new ObjectId(realEstate.id).toHexString()));
+    const potentialRealEstates = allRealEstates
+      .filter((realEstate) => !actualRealEstateIDs.has(new ObjectId(realEstate.id).toHexString()));
+
+    return {
+      actualMatches: actualRealEstates,
+      potentialMatches: potentialRealEstates,
+    };
   }
+
+  private async findLandlordsUserMatches(landlord: User) : Promise<UserMatches<TenantBiography>> {
+    const landlordsRealEstateIDs = (landlord.bio as LandlordBiography)
+      .realEstates.map((realEstate) => new ObjectId(realEstate.id).toHexString());
+    const landlordsRealEstateIDsSet = new Set(landlordsRealEstateIDs);
+    const allMatches = await this.matchRepo.find();
+    const actualMatches = allMatches.filter(
+      (match) => landlordsRealEstateIDsSet.has(match.realEstateID.toHexString()),
+    );
+
+    const actualTenantsIDsSet = new Set(actualMatches.map(
+      (match) => new ObjectId(match.tenantUserID).toHexString(),
+    ));
+
+    const allTenants = await this.users.find({ mode: UserMode.Tenant });
+
+    const actualTenants = allTenants.filter(
+      (tenant) => actualTenantsIDsSet.has(new ObjectId(tenant.bio.id).toHexString()),
+    );
+
+    const actualTenantsBiographies = actualTenants
+      .map((user) => user.bio as TenantBiography);
+
+    const potentialTenants = allTenants.filter(
+      (tenant) => !actualTenantsIDsSet.has(new ObjectId(tenant.bio.id).toHexString()),
+    );
+    const potentialMatchesBiographies = potentialTenants
+      .map((match) => match.bio as TenantBiography);
+
+    return {
+      actualMatches: actualTenantsBiographies,
+      potentialMatches: potentialMatchesBiographies,
+    };
+  }
+}
+
+interface UserMatches<T extends (RealEstate | TenantBiography)> {
+  actualMatches: T[];
+  potentialMatches: T[];
 }
